d33cc3f37635393786d8fa9eda23fd978e09e6f1
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-04 12:01:49 +0800

    feat: 增加 layout 和 state

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'plain',
+})
+
+const isLoggedIn = useIsLoggedIn()
+</script>
 <template>
-  <div class="flex items-center justify-center h-screen -mt-36">
+  <div class="flex items-center justify-center h-screen">
     <div>
       <div class="badge badge-lg badge-primary badge-outline">Nuxt App</div>
       <h1 class="text-2xl">The Ultimate Guide to Vue Performance</h1>
     </div>
+    <div>{{ isLoggedIn }}</div>
   </div>
 </template>
