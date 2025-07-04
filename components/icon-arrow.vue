65c8ead6feac9c135907a2a7fe286cfc9fb3ce54
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-04 18:31:16 +0800

    feat: 增加按钮demo

---
+++ components/icon-arrow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg
+    width="17"
+    height="12"
+    viewBox="0 0 17 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      opacity="0.9"
+      d="M1.68164 10.3613L8.68164 2.36133L15.6816 10.3613"
+      stroke="white"
+      stroke-width="2"
+    />
+  </svg>
+</template>
+
+<style scoped></style>
---
+++ pages/buttons.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="grid grid-cols-2 grid-rows-2 gap-2 w-[128px] aspect-square bg-white rounded-full overflow-clip relative"
+  >
+    <div
+      v-for="i of 4"
+      :key="i"
+      class="hover:bg-amber-500 cursor-pointer bg-gray-300 flex items-center justify-center"
+    >
+      <IconArrow
+        :style="{ '--no': i }"
+        class="rotate-[calc(90deg*var(--no)-135deg)]"
+      />
+    </div>
+
+    <div
+      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white w-[calc(50%_+_0.25rem)] aspect-square rounded-full overflow-clip p-2"
+    >
+      <div
+        class="hover:bg-amber-500 cursor-pointer bg-gray-300 aspect-square rounded-full"
+      >
+        5
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
