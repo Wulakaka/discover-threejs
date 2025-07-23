61a628ae7ceffc5f7ed1e0bdbbc0838c49e90912
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-23 15:20:11 +0800

    feat: 增加自定义动画

--- pages/three/models/World/components/birds/setupModel.ts
+++ pages/three/models/World/components/birds/setupModel.ts
@@ -1,4 +1,4 @@
-import { AnimationMixer } from 'three'
+import { AnimationClip, AnimationMixer, VectorKeyframeTrack } from 'three'
 import type { GLTF } from 'three/examples/jsm/Addons.js'
 
 function setupModel(data: GLTF) {
@@ -7,9 +7,34 @@ function setupModel(data: GLTF) {
 
   const mixer = new AnimationMixer(model)
   const action = mixer.clipAction(clip)
+  // action.startAt(2).setEffectiveTimeScale(0.5).play().halt(10)
   action.play()
 
-  model.tick = (delta: number) => mixer.update(delta)
+  // 创建动画轨迹，不同的属性需要使用不同的轨迹类
+  const positionTrack = new VectorKeyframeTrack(
+    '.position',
+    [0, 1, 2],
+    [0, 0, 0, 0, 5, 0, 0, 0, 4]
+  )
+
+  const scaleTrack = new VectorKeyframeTrack(
+    '.scale',
+    [0, 1, 3],
+    [0.1, 0.1, 0.1, 1, 1, 1, 0.1, 0.1, 0.1].map((v) => v * 0.01)
+  )
+  // 创建新的动画剪辑
+  const clipMove = new AnimationClip('move', -1, [positionTrack, scaleTrack])
+  // turn it into an animated mesh by connecting it to a mixer
+  // 通过连接到一个混合器将其转换为一个动画网格
+  const mixerMove = new AnimationMixer(model)
+  // 创建动画动作
+  const actionMove = mixerMove.clipAction(clipMove)
+  actionMove.play()
+
+  model.tick = (delta: number) => {
+    mixer.update(delta)
+    mixerMove.update(delta)
+  }
 
   return model
 }
