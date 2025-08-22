bf6ab684dad180546672208701cf98960e4c148f
zhang.weifeng <zhangweifeng@diinom.com>
2025-08-22 17:55:11 +0800

    feat: tip 线改为 line

--- pages/3d/components/Main.vue
+++ pages/3d/components/Main.vue
@@ -2,14 +2,18 @@
 import { OrbitControls } from '@tresjs/cientos'
 
 import StackingSurface from './StackingSurface.vue'
+
+// import OrbitControls from './OrbitControls.vue'
+
+const controlsRef = shallowRef()
 </script>
 
 <template>
   <TresCanvas window-size>
-    <TresPerspectiveCamera ref="cameraRef" :position="[0, 70, 0]" />
-    <OrbitControls />
+    <TresPerspectiveCamera :position="[0, 70, 0]" :args="[45, 1, 0.1, 1000]" />
+    <OrbitControls ref="controlsRef" />
     <TresGroup :position="[-60, 0, -20]">
-      <StackingSurface />
+      <StackingSurface :controls="controlsRef" />
     </TresGroup>
     <TresAmbientLight :intensity="2" />
     <TresDirectionalLight :position="[2, 2, 2]" :intensity="5" />
